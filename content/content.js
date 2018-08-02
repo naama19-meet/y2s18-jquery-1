7184b13a39abd25d175e2378d8b2ad996fb06239
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 22:54:54 +0300

    classes.js

--- content/content.js
+++ content/content.js
@@ -1,1 +1,21 @@
-// Write all your JavaScript and JQuery code in this file! :)
+var a = 0;
+
+
+
+
+
+
+$("#counter").on("click", function(){
+	
+	a++;
+	$("h1").eq(1).text(a);
+
+});
+
+
+$("#reset").on("click", function(){
+	a=0;
+
+
+});
+
