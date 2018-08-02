e34ac908ee6c1686985a5f667de51fcabe2c7ef4
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:32:47 +0300

    jquery1

--- events/events.js
+++ events/events.js
@@ -10,3 +10,16 @@ function randomY() {
 /* END PROVIDED CODE */
 
 // Write your code below this line :)
+
+
+    
+
+
+
+    
+
+$("#moveButton").on("click", function() {
+	$("#message").css("left", randomX());
+	$("#message").css("right", randomY());
+})
+
--- style/style.js
+++ style/style.js
@@ -1,6 +1,9 @@
 // Write all your JavaScript and JQuery code in this file! :)
 
+ 
 
+
+$("#img").eq(2) = this.valeu
 
 function updateSize(newSize) {
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
